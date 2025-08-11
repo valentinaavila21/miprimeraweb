4aaee4c5cd5da24a4f0923d7bd6d1e76172aeb31
Valentina Ávila P. <valentina.avila.p@ug.uchile.cl>
2025-08-11 16:22:52 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var d; 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(26,182,240);
+}
+
+function draw() {
+  d=random(10,60)
+  fill(255,random(55,200));
+  ellipse(mouseX,mouseY,d,d)
+}
