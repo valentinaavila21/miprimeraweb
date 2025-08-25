99c09ecc662b7f393423b6368c62552ac42c0299
Valentina Ávila P. <valentina.avila.p@ug.uchile.cl>
2025-08-25 00:49:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,14 @@ var d;
 
 function setup() {
  createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
-  background(26,182,240);
+ colorMode(RGB, 255, 255, 255, 255);
+  background(26,182,240,50);
 }
 
 function draw() {
   d = random(10,60)
   colorMode(HSB,360,100,100);
+  noStroke();
   fill(random(0,360),100,100);
   ellipse(mouseX,mouseY,d,d)
 }
